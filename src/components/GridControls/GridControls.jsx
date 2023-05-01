f379fd0e979d5f23f9b529072ee249134bfa1933
geelona <mr.geelona@gmail.com>
2023-05-01 17:33:47 +0300

    the project is stylized

--- src/components/GridControls/GridControls.jsx
+++ src/components/GridControls/GridControls.jsx
@@ -5,14 +5,14 @@ function GridControls(props) {
     <>
     <div className={s.GridControls}>
       <div className={s.AxisX}>
-        <input placeholder='X axis' value={props.xAxis} onChange={(e) => {
+        <input className={s.input} placeholder='X axis' value={props.xAxis} onChange={(e) => {
           !isNaN(e.target.value) ? props.setXAxis(e.target.value) : ''
-        }}/> - X
+        }}/>
       </div>
       <div className={s.AxisY}>
-        <input placeholder='Y axis' value={props.yAxis} onChange={(e) => {
+        <input className={s.input} placeholder='Y axis' value={props.yAxis} onChange={(e) => {
           !isNaN(e.target.value) ? props.setYAxis(e.target.value) : ''
-        }}/> - Y
+        }}/>
       </div>
     </div>
     </>
